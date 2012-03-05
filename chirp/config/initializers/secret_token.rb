67862b15e0da8927adb800a7a7a504faa6666df7
Rajiv Bakulesh Shah <brainix@gmail.com>
2012-03-05 03:32:44 -0600

    created rails 3.2 webapp

---
+++ chirp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chirp::Application.config.secret_token = '96400e4a6327c8f91485250f360eab06708471e55fd6f6804ae85449f7ac257747e135b75ac8ec8d17a49ec1b26755c7446e8a6e5fada4b4da9669d1e3271a73'
