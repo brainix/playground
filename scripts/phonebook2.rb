2da58fecb42a4845cdaa25ed25aceaa6c9df1c0a
Raj Shah <brainix@gmail.com>
2012-03-13 15:07:27 -0500

    wrote phonebook2.rb (demonstrates method_missing and object message sending for dynamic method invocation)

--- scripts/phonebook2.rb
+++ scripts/phonebook2.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 #----------------------------------------------------------------------------#
-#   phonebook1.rb                                                            #
+#   phonebook2.rb                                                            #
 #                                                                            #
 #   Copyright (c) 2012, Rajiv Bakulesh Shah, original author.                #
 #                                                                            #
@@ -21,35 +21,25 @@
 #----------------------------------------------------------------------------#
 
 
-# This PhoneBook class is a wrapper around the Node class.  The interesting
-# stuff is in the Node class below.
 class PhoneBook
 
   def initialize
     @node = Node.new
   end
 
-  def add(s)
-    s = validate_input(s)
-    @node.add(s)
-    self
-  end
-
-  def delete(s)
-    s = validate_input(s)
-    @node.delete(s)
-    self
-  end
-
-  def include?(s)
-    s = validate_input(s)
-    @node.include?(s)
+  def method_missing(name, *args, &block)
+    if ['add', 'delete', 'include?'].include?(name.to_s)
+      s = validate_input(*args)
+      val = @node.send(name, *args)
+      val = name.to_s == 'include?' ? val : self
+      return val
+    else
+      return super(name, *args, &block)
+    end
   end
 
   private
 
-  # Validate phone number input.  Ensure it's of the form: 713-725-8220  Then
-  # strip out the hyphens.
   def validate_input(s)
     regex = /^\d{3}-\d{3}-\d{4}$/
     match = s =~ regex
@@ -59,31 +49,6 @@ class PhoneBook
     s.gsub(/[^0-9]/, '')
   end
 
-  # This Node class implements an n-ary tree that represents a phone book.  The
-  # tree allows us to add, look up, and remove phone numbers.
-  #
-  # Each node in the tree represents one digit in at least one phone number.
-  # The memory win, though, is that a node could represent a digit in multiple
-  # phone numbers.  If two phone numbers start with the same area code, their
-  # traversal paths down the tree start with the same three nodes, then
-  # diverge.  The full phone numbers aren't stored anywhere in the tree, but
-  # can be derived through their traversal paths.  Each path down the tree
-  # represents one phone number.
-  #
-  # This implementation is elegant and memory-efficient, but has some
-  # drawbacks:
-  #   1.  No duplicate phone numbers.
-  #   2.  No way to differentiate a part from a whole.
-  #       A.  Add 713-725-8220, then look up 713 - the tree reports that 713
-  #           exists.
-  #       B.  Add 713, then add 713-725-8220, then remove 713-725-8220, then
-  #           look up 713 - the tree reports that 713 doesn't exist.
-  #   3.  No way to meaningfully sort phone numbers.
-  #   4.  Recursion probably isn't the most efficient approach.
-  #
-  # However, we're targetting well ordered data (only 10-digit phone numbers,
-  # not arbitrary strings) for a specific use case.  So for us, the advantages
-  # outweigh the disadvantages.
   class Node
 
     attr_reader :children
