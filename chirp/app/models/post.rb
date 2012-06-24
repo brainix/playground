f30ecef142c88e6c28f9c4595ae557d47bf3ca22
Raj Shah <brainix@gmail.com>
2012-06-24 06:14:56 -0700

    removed useless directories

--- chirp/app/models/post.rb
+++
@@ -1,4 +0,0 @@
-class Post < ActiveRecord::Base
-  validates :body, :presence => true,
-                   :length => { :maximum => 140 }
-end
--- duckie/config.ru
+++
@@ -1,3 +0,0 @@
-require './duckie'
-
-run Sinatra::Application
